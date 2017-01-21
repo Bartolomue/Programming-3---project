3751d1928c630d4af12e8f731e801a4d6a3ad0ba
Bartolomeu <bartekwichowski@gmail.com>
2017-01-21 14:07:34 +0100

    Some libraries.

--- src/Data.java
+++ src/Data.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,17 +9,22 @@ import java.util.List;
  */
 public class Data {
 
-    //Test commit
-
-
-
     public List<DocumentCustom> docs = new ArrayList<DocumentCustom>();
 
-    public Data() throws IOException {
+    public void getDataFromWeb() throws IOException {
         docs.add(new DocumentCustom(SiteDownloader.getContent(SiteDownloader.getSampleUrl1())));
         docs.add(new DocumentCustom(SiteDownloader.getContent(SiteDownloader.getSampleUrl2())));
         docs.add(new DocumentCustom(SiteDownloader.getContent(SiteDownloader.getSampleUrl3())));
         docs.add(new DocumentCustom(SiteDownloader.getContent(SiteDownloader.getSampleUrl4())));
         docs.add(new DocumentCustom(SiteDownloader.getContent(SiteDownloader.getSampleUrl5())));
+    }
+
+    public void getDataLocalFiles() throws IOException {
+        docs.add(new DocumentCustom(getStringFromFile("data/Boston")));
+        docs.add(new DocumentCustom(getStringFromFile("data/New_York")));
+    }
+
+    public String getStringFromFile(String pathToFile) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(pathToFile)));
     }
 }
--- src/TestMain3.java
+++ src/TestMain3.java
@@ -6,8 +6,19 @@ import java.io.IOException;
 public class TestMain3 {
 
     public static void main(String argv[]) throws IOException {
-        CosineTextSimilarity c = new CosineTextSimilarity("name", "name a");
+
+        Data d = new Data();
+        d.getDataLocalFiles();
+
+        String s1 = d.docs.get(0).content;
+        String s2 = d.docs.get(1).content;
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+        CosineTextSimilarity c = new CosineTextSimilarity("b a", "a b");
         double test = c.getCosineSimilarity();
+
         System.out.println(test);
     }
 }
