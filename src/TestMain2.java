583d501b8c91c6c693a66237f8b51614f6743607
MrMaks <maksym.stec@gmail.com>
2017-01-24 21:26:18 +0100

    Addtest saves

--- src/TestMain2.java
+++ src/TestMain2.java
@@ -10,7 +10,7 @@ public class TestMain2 {
         v= new Visualization();
         try {
             //SampleData.createFilesFromWeb(); - patrzcie zakomentowane a działa
-            Integer option=1; // // 1=load, 2=create and save, 3=regular
+            Integer option=2; // // 1=load, 2=create and save, 3=regular
             // Load from user
             String IP = "";
             int portNumber = 6066;
@@ -29,7 +29,8 @@ public class TestMain2 {
             }
             else if(option == 2)
             {
-                v = new Visualization(SampleData.getSampleNotesFromFiles(IP, portNumber), 0.85, 4);
+                v = new Visualization(SampleData.getSampleNotesFromFiles(), 0.85, 4);
+
                 v.saveGraph("TestFile99");
                 System.out.println("Visualization done.");
             }
@@ -49,10 +50,7 @@ public class TestMain2 {
 
         if (v != null) {
             v.drawGraph();
-            Gui g = new Gui();
-            g.add(v.getView());
-            g.setVisible(true);
-            //v.drawGraph();
+
         }
     }
 }
